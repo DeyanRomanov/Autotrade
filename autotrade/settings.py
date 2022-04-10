d42805ebeae69dd9dcc1509f5407826f175626fa
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-10 18:18:50 +0300

    Fix migration

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'admin_honeypot',
+    # 'admin_honeypot',
 
     'autotrade.userapp',
     'autotrade.products',
