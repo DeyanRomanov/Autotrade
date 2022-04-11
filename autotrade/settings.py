103d11a389251f59590f33c468668363af45b4a5
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-11 09:18:35 +0300

    Fix static files

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -67,6 +67,17 @@ WSGI_APPLICATION = 'autotrade.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'autotrade_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Snejieli90@',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -121,7 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'templates/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'templates/static',
