4adfd3f4f6de798ed4990fc4ef54038bebad652c
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-28 18:48:28 +0300

    change staticfile_dirs

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'templates/static',
+    BASE_DIR / 'static',
 )
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
