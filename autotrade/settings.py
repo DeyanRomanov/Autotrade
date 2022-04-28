43fd396795ad6f5376be11dc75b1c1b0636e0ddc
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-28 18:57:35 +0300

    change staticfile_dirs

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'templates/static',
 )
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
