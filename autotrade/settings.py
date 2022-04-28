e540817b7e4a1766dd737c016c90bd2b81e5cb0f
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-28 20:20:37 +0300

    copy images in staticfiles

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = (
     BASE_DIR / 'templates/static',
 )
-MEDIA_ROOT = BASE_DIR / 'staticfiles/mediafiles'
-MEDIA_URL = '/staticfiles/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_USE_FINDERS = True
