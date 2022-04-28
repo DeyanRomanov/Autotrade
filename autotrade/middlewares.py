1d13c5ce5f9b23d031c04896af3eb1ac71f31e2d
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-28 21:16:45 +0300

    change debug var to True in dev

--- autotrade/middlewares.py
+++ autotrade/middlewares.py
@@ -6,7 +6,7 @@ from autotrade.views import InternalErrorView
 def handle_exception(get_response):
     def middleware(request):
         response = get_response(request)
-        if response.status_code >= 500:
+        if response.status_code >= 405:
             logging.critical('Critical')
             logging.error('Info')
             logging.warning('Warning')
