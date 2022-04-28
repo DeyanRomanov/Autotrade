013eed944730fa19033f4280fd0843ffe42d2988
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-28 18:41:09 +0300

    change withnoise

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
 )
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
