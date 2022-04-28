d54b36d7424f74400c2ff1897e65790aa8242b03
DeyanRomanov <deyan_romanov@gmail.com>
2022-04-28 19:12:05 +0300

    add install apps whitenoise

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 
     'admin_honeypot',
 
@@ -39,9 +40,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'autotrade.middlewares.handle_exception',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
