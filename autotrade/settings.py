c5a6c9f52ff2198a51552916b352091e19d0bebb
DeyanRomanov <deyan_romanov@gmail.com>
2022-05-12 15:31:35 +0300

    fix rediss options in settings.py

--- autotrade/settings.py
+++ autotrade/settings.py
@@ -95,6 +95,12 @@ CACHES = {
             if DEBUG
             else 'django_redis.cache.RedisCache',
         'LOCATION': os.getenv('REDIS_URL', 'redis://127.0.0.1:6379'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {
+                "ssl_cert_reqs": None
+            },
+        }
     }
 }
 
